daf745cbb97e9052635c9d1acbc9e3be4a545063
GuillaumeGibert <ggibert@gmail.com>
2019-06-11 18:08:55 +0200

    +Add Serial event reading

--- testArduino/testArduino.ino
+++ testArduino/testArduino.ino
@@ -1,11 +1,16 @@
 const int _nbPinIn = 5;
 const int _portBaud = 9600;
 const float _fps = 10.0;
+String inputString = "";         // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
 
 void setup() 
 {
   // initializes serial port
   Serial.begin(_portBaud);
+
+  // reserves 200 bytes for the inputString:
+  inputString.reserve(200);
 }
 
 void loop() 
@@ -13,6 +18,7 @@ void loop()
     // initialzes the timer
     unsigned long startTime = micros();
 
+    
     Serial.print("Sensor:|");
   
     for (int l_pinIn = 0; l_pinIn < _nbPinIn; l_pinIn++)
@@ -26,6 +32,16 @@ void loop()
 
     Serial.print("\n");
 
+
+    // print the string when a newline arrives:
+    if (stringComplete) 
+    {
+      Serial.println(inputString);
+      // clear the string:
+      inputString = "";
+      stringComplete = false;
+    }
+
     // estimates the loop duration and waits before starting a new loop
     unsigned long currentTime = micros(); // in microseconds
     unsigned long elapsedTime = currentTime - startTime; // in microseconds
@@ -36,4 +52,23 @@ void loop()
     else
       delay(waitingTime / 1000); // in milliseconds 
  
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
 }
